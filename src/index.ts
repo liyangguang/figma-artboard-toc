08416d2f9f6ee2b349ed18bd3bc50156045eb5c0
Yangguang Li <liyangguangcn@gmail.com>
2021-12-26 13:10:40 -0800

    Add clientStorage.

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,13 @@ import {renderCover} from './cover';
 import {createPageSections, parseExistingPages} from './page_sections';
 import {focusToPage} from './helpers';
 
+interface PostMessageData {
+  initialSectionTitles?: string[];
+  error?: string;
+}
+
+const CLIENT_STORAGE_SECTIONS_KEY = 'sections';
+
 (async function init() {
   await Promise.all(Array.from(FONTS_MAP.values()).map((font) => figma.loadFontAsync(font)));
 
@@ -12,7 +19,9 @@ import {focusToPage} from './helpers';
 
     figma.showUI(__html__);
 
-    figma.ui.postMessage({existingSections: existingSections.length ? existingSections : DEFAULT_SECTION_TITLES});
+    const initialSectionTitles = await getInitialTitleList(existingSections);
+
+    figma.ui.postMessage({initialSectionTitles} as PostMessageData);
 
     figma.ui.onmessage = (message) => {
       try {
@@ -20,7 +29,7 @@ import {focusToPage} from './helpers';
         figma.closePlugin();
       } catch (error) {
         console.log(error.message)
-        figma.ui.postMessage({error: error.message});
+        figma.ui.postMessage({error: error.message} as PostMessageData);
       }
     }
   } catch (error) {
@@ -33,5 +42,15 @@ function createAndUpdate(sectionTitles: string[]): void {
   renderCover();
   renderToc(sectionTitles);
 
-  focusToPage(TOC_PAGE_NAME)
+  focusToPage(TOC_PAGE_NAME);
+  figma.clientStorage.setAsync(CLIENT_STORAGE_SECTIONS_KEY, sectionTitles);
+}
+
+async function getInitialTitleList(existingSections: string[]): Promise<string[]> {
+  if (existingSections?.length) return existingSections;
+
+  const savedValue = await figma.clientStorage.getAsync(CLIENT_STORAGE_SECTIONS_KEY);
+  if (savedValue) return savedValue;
+
+  return DEFAULT_SECTION_TITLES;
 }
