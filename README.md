efd15db4a2c3aab2584ae13df470d8ba51a93f78
Yangguang Li <liyangguangcn@gmail.com>
2021-12-26 13:24:17 -0800

    Update documentation, and include /dist

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
-A figma plugin to generate table of contents of all artboards, with links to each one.
+# ToC+
 
-## How to develop
+A figma plugin to:
+- Generate pages by sections
+- Generate table-of-contents of section > page > artboard, with links directly to the item
 
-- Compile TS with watching: `npm start`
-- Compile TS: `npm build`
+## Load the plugin manually
 
-## TODOs
+This is only needed before the plugin is published, or you want to run a modified local version.
 
-- A UI to edit settings. With save to local
-- page description (?)
+1. Download or clone this repo.
+1. Install the desktop version of Figma (web version doesn't support loading local plugins).
+1. Open any Figma Design file.
+1. Right click anywhere in the working area, `Plugins > Development > Import plugin from manifest`.
+1. Select the `manifest.json` file at the root of this repo.
+1. Now you can start use it: `Plugins > Development > ToC+`.
+
+## Development
+
+- Main code all under `/src`:
+  - `ui.html` for the UI part (with `<style>` and `<script>` inside).
+  - All the `.ts` files for the code part.
+  - DO NOT modify files under `/dist`, those are generated files, and will be overwritten everytime you compile.
+- Bundled using `rollup.js`: (After editing source code, you must run either of these)
+  - Compile TS with auto re-run on saving: `npm start`.
+  - Compile TS once: `npm build`.
+- Final code:
+  - compiled under `/dist`
+  - `manifest.json`: the Figma Plugin entry point.
---
+++ dist/index.js
@@ -0,0 +1,301 @@
+'use strict';
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation.
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+***************************************************************************** */
+
+function __awaiter(thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+}
+
+// Page names
+const COVER_PAGE_NAME = '🌌 Cover';
+const TOC_PAGE_NAME = '📖 Table of contents';
+const UNCATEGORIZED_PAGE_NAME = 'Un-categorized pages';
+const DIVIDE_LING_PAGE_NAME = '________________________________________';
+const PAGE_PREFIX = '↳';
+const PAGE_NAMES_TO_IGNORE = [COVER_PAGE_NAME, TOC_PAGE_NAME, UNCATEGORIZED_PAGE_NAME, DIVIDE_LING_PAGE_NAME];
+const DEFAULT_SECTION_TITLES = [
+    '🏁 Ready',
+    '✏️ Work in progress',
+    '🔎 Research',
+    '_💡 Ideas',
+];
+// Styles
+const FONT_SIZE_BASE = 14;
+const INDENTATION_WIDTH = FONT_SIZE_BASE * 2;
+var FontEnum;
+(function (FontEnum) {
+    FontEnum[FontEnum["REGULAR"] = 0] = "REGULAR";
+    FontEnum[FontEnum["BOLD"] = 1] = "BOLD";
+})(FontEnum || (FontEnum = {}));
+const FONTS_MAP = new Map([
+    [FontEnum.REGULAR, { family: 'Roboto', style: 'Regular' }],
+    [FontEnum.BOLD, { family: 'Roboto', style: 'Bold' }],
+]);
+
+function createPage(name, checkExisting = true) {
+    const existingPage = findChildByName(name);
+    if (checkExisting && existingPage)
+        return existingPage;
+    const page = figma.createPage();
+    page.name = name;
+    return page;
+}
+function movePage(page, index) {
+    const pageNode = typeof page === 'string' ? findChildByName(page) : page;
+    figma.root.insertChild(index, pageNode);
+}
+function findChildByName(name, parent = figma.root) {
+    return parent.findChild((node) => node.name === name);
+}
+function appendFrame(parent, name, isAutoLayout = false) {
+    const frame = figma.createFrame();
+    frame.name = name;
+    if (isAutoLayout) {
+        frame.layoutMode = 'VERTICAL';
+        frame.counterAxisSizingMode = 'AUTO';
+        frame.itemSpacing = FONT_SIZE_BASE * .5;
+        frame.verticalPadding = FONT_SIZE_BASE * .4;
+        frame.horizontalPadding = FONT_SIZE_BASE * .8;
+    }
+    parent.appendChild(frame);
+    return frame;
+}
+function appendTextNode(parent, name, linkId = null, isLargerFont = false) {
+    const textNode = figma.createText();
+    textNode.characters = name.trim();
+    textNode.fontSize = FONT_SIZE_BASE * (isLargerFont ? 1.2 : 1);
+    textNode.fontName = FONTS_MAP.get(isLargerFont ? FontEnum.BOLD : FontEnum.REGULAR);
+    if (linkId) {
+        textNode.hyperlink = { type: 'NODE', value: linkId };
+        textNode.textDecoration = 'UNDERLINE';
+    }
+    parent.appendChild(textNode);
+    return textNode;
+}
+function focusToPage(name) {
+    figma.currentPage = findChildByName(name);
+    figma.viewport.scrollAndZoomIntoView(figma.currentPage.children);
+}
+
+const TOC_FRAME_NAME = 'table of contents';
+function renderToc(sectionTitles) {
+    const tocPage = findChildByName(TOC_PAGE_NAME);
+    const tocFrame = tocPage.findChild((node) => node.type === 'FRAME' && node.name === TOC_FRAME_NAME);
+    if (tocFrame) {
+        tocFrame.remove();
+    }
+    const newTocFrame = appendFrame(tocPage, TOC_FRAME_NAME, true);
+    renderTocContent(newTocFrame, sectionTitles);
+    if (!newTocFrame.children.length) {
+        appendTextNode(newTocFrame, 'Table of contents is empty', null, true);
+        appendTextNode(newTocFrame, `All pages are created. Now it's design time!`);
+        appendTextNode(newTocFrame, 'Run the plugin again anytime to refresh this ToC, and the last updated time in the cover.');
+    }
+    return newTocFrame;
+}
+function renderTocContent(tocFrame, sectionTitles) {
+    const sections = groupsPagesIntoSections(sectionTitles);
+    for (const section of sections) {
+        if (section.title.startsWith('_'))
+            continue;
+        const nonHiddenPagesInThisSection = section.pages.filter((page) => !page.name.startsWith('_'));
+        if (!nonHiddenPagesInThisSection.length)
+            continue;
+        // Add section frame and title
+        const sectionFrame = appendFrame(tocFrame, `${section.title} section`, true);
+        appendTextNode(sectionFrame, section.title, null, true);
+        for (const page of nonHiddenPagesInThisSection) {
+            // Ignore empty page, and pages under hidden sections
+            const artboardsInThisPage = page.children.filter((node) => node.type === 'FRAME' && !node.name.startsWith('_'));
+            if (!artboardsInThisPage.length)
+                continue;
+            // Add page frame and title
+            const pageAutoLayoutFrame = appendFrame(sectionFrame, `${page.name} page`, true);
+            appendTextNode(pageAutoLayoutFrame, page.name.replace(PAGE_PREFIX, ''), page.id);
+            // Add all the artboards
+            const artboardListAutoLayoutFrame = appendFrame(pageAutoLayoutFrame, `${page.name} artboards`, true);
+            artboardListAutoLayoutFrame.paddingLeft = INDENTATION_WIDTH;
+            for (const frame of artboardsInThisPage) {
+                const artboardNode = appendTextNode(artboardListAutoLayoutFrame, frame.name, frame.id);
+                artboardNode.x = INDENTATION_WIDTH;
+            }
+        }
+        removeFrameWithoutChildren(sectionFrame);
+    }
+}
+function groupsPagesIntoSections(sectionTitles) {
+    const result = [];
+    let currentSection;
+    for (const page of figma.root.children.filter((page) => !PAGE_NAMES_TO_IGNORE.includes(page.name))) {
+        if (sectionTitles.includes(page.name)) {
+            currentSection = { title: page.name, pages: [] };
+            result.push(currentSection);
+            continue;
+        }
+        if (!currentSection) {
+            currentSection = { title: 'Ungrouped pages', pages: [] };
+            result.push(currentSection);
+        }
+        currentSection.pages.push(page);
+    }
+    return result;
+}
+function removeFrameWithoutChildren(node) {
+    if (!node)
+        return;
+    if (node.children.length === 1 && node.children[0].type === 'TEXT') {
+        node.remove();
+    }
+}
+
+const COVER_FRAME_NAME = 'cover';
+const LAST_UPDATED_NAME = 'Last updated';
+const COVER_DIMENSIONS = [1024, 576];
+const PADDING = 16;
+function renderCover() {
+    const coverPage = findChildByName(COVER_PAGE_NAME);
+    let coverFrame = coverPage.findChild((node) => node.type === 'FRAME' && node.name === COVER_FRAME_NAME);
+    if (!coverFrame) {
+        coverFrame = appendFrame(coverPage, COVER_FRAME_NAME);
+        coverFrame.resize(COVER_DIMENSIONS[0], COVER_DIMENSIONS[1]);
+        renderCoverFrame(coverFrame);
+    }
+    updateLastUpdated(coverFrame);
+    return coverFrame;
+}
+function updateLastUpdated(frame) {
+    const textNode = frame.findChild((node) => node.name === LAST_UPDATED_NAME) || appendTextNode(frame, LAST_UPDATED_NAME);
+    textNode.name = LAST_UPDATED_NAME;
+    textNode.characters = `${LAST_UPDATED_NAME}: ${new Date().toLocaleDateString()}`;
+    textNode.x = COVER_DIMENSIONS[0] - textNode.width - PADDING;
+    textNode.y = COVER_DIMENSIONS[1] - textNode.height - PADDING;
+}
+function renderCoverFrame(frame) {
+    const placeholderNode = appendTextNode(frame, 'Put your cover here');
+    placeholderNode.fontSize = 60;
+    placeholderNode.x = (COVER_DIMENSIONS[0] - placeholderNode.width) / 2;
+    placeholderNode.y = (COVER_DIMENSIONS[1] - placeholderNode.height) / 2;
+    const tocPage = findChildByName(TOC_PAGE_NAME);
+    if (tocPage) {
+        const tocNode = appendTextNode(frame, 'Table of contents', tocPage.id);
+        tocNode.x = COVER_DIMENSIONS[0] - tocNode.width - PADDING;
+        tocNode.y = COVER_DIMENSIONS[1] - tocNode.height * 2 - PADDING * 2;
+    }
+}
+
+const EMPTY_PAGE_MESSAGE = 'This page is intentionally empty.';
+function createPageSections(sectionTitle) {
+    // Check before inserting any pages
+    const emptyPageName = checkEmptyFile();
+    // Insert pages
+    insertCoverAndToc();
+    insertSections(sectionTitle);
+    focusToPage(TOC_PAGE_NAME);
+    // Must remove focus first then remove
+    if (emptyPageName) {
+        findChildByName(emptyPageName).remove();
+    }
+}
+function parseExistingPages() {
+    let isPreviousDividingLine = false;
+    const titles = [];
+    for (const page of figma.root.children) {
+        if ([TOC_PAGE_NAME, COVER_PAGE_NAME].includes(page.name))
+            continue;
+        if (page.name === DIVIDE_LING_PAGE_NAME) {
+            isPreviousDividingLine = true;
+            continue;
+        }
+        else {
+            if (isPreviousDividingLine) {
+                titles.push(page.name);
+            }
+            isPreviousDividingLine = false;
+        }
+    }
+    return titles;
+}
+function checkEmptyFile() {
+    const isEmptyFile = figma.root.children.length === 1 && !figma.root.children[0].children.length;
+    return isEmptyFile ? figma.root.children[0].name : null;
+}
+function insertCoverAndToc() {
+    createPage(COVER_PAGE_NAME);
+    createPage(TOC_PAGE_NAME);
+    movePage(COVER_PAGE_NAME, 0);
+    movePage(TOC_PAGE_NAME, 1);
+}
+function insertSections(sectionTitle) {
+    for (const name of sectionTitle) {
+        if (!findChildByName(name)) {
+            const dividingPage = createPage(DIVIDE_LING_PAGE_NAME, false);
+            appendTextNode(dividingPage, `${EMPTY_PAGE_MESSAGE} It's just a dividing line for page sections.`);
+            const titlePage = createPage(name);
+            appendTextNode(titlePage, `${EMPTY_PAGE_MESSAGE} It's just a the title of page sections.`);
+            createPage(`${PAGE_PREFIX} [Your page 1]`, false);
+            createPage(`${PAGE_PREFIX} [Your page 2]`, false);
+        }
+    }
+}
+
+const CLIENT_STORAGE_SECTIONS_KEY = 'sections';
+(function init() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield Promise.all(Array.from(FONTS_MAP.values()).map((font) => figma.loadFontAsync(font)));
+        try {
+            const existingSections = parseExistingPages();
+            figma.showUI(__html__);
+            const initialSectionTitles = yield getInitialTitleList(existingSections);
+            figma.ui.postMessage({ initialSectionTitles });
+            figma.ui.onmessage = (message) => {
+                try {
+                    createAndUpdate(message);
+                    figma.closePlugin();
+                }
+                catch (error) {
+                    console.log(error.message);
+                    figma.ui.postMessage({ error: error.message });
+                }
+            };
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+})();
+function createAndUpdate(sectionTitles) {
+    createPageSections(sectionTitles);
+    renderCover();
+    renderToc(sectionTitles);
+    focusToPage(TOC_PAGE_NAME);
+    figma.clientStorage.setAsync(CLIENT_STORAGE_SECTIONS_KEY, sectionTitles);
+}
+function getInitialTitleList(existingSections) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (existingSections === null || existingSections === void 0 ? void 0 : existingSections.length)
+            return existingSections;
+        const savedValue = yield figma.clientStorage.getAsync(CLIENT_STORAGE_SECTIONS_KEY);
+        if (savedValue)
+            return savedValue;
+        return DEFAULT_SECTION_TITLES;
+    });
+}
